7d7c2c0b6fb0f8331c230ac1faee982787bbb8d6
shahzadishq <shezigull5@gmail.com>
2022-06-12 12:13:20 +0500

    Updates

---
+++ precache-manifest.d6e605b8c57cb20d62772b6548c15a8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d98928e66c891986f23",
+    "url": "/utility/static/css/main.7510fa88.chunk.css"
+  },
+  {
+    "revision": "4d98928e66c891986f23",
+    "url": "/utility/static/js/main.4d98928e.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/utility/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/utility/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "d5a088bbd9af8c4702c8",
+    "url": "/utility/static/js/runtime~main.d5a088bb.js"
+  },
+  {
+    "revision": "c28d224554c6b20075d045a818b06707",
+    "url": "/utility/index.html"
+  }
+];
